6966bc9d8074a1f364426f8b788ee09c416577f2
badasswp <badasswpdev@gmail.com>
2025-10-20 06:34:40 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/addon-for-post-meta-translation-using-deepl',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '85b0ace7c3fd6e2f753d066a338bd60a7b9dc3ed',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/addon-for-post-meta-translation-using-deepl' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '85b0ace7c3fd6e2f753d066a338bd60a7b9dc3ed',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
