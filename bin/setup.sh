3b014eda53bf9bb3e6e4ec2ab37704dc5362ee12
badasswp <badasswpdev@gmail.com>
2026-05-25 12:25:41 +0000

    push to master-built

--- bin/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wp-env run cli wp theme activate twentytwentythree
-wp-env run cli wp rewrite structure /%postname%
-wp-env run cli wp option update blogname "Addon for Post Meta Translation using DeepL"
-wp-env run cli wp option update blogdescription "Translate post meta data when using DeepL translate."
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/addon-for-post-meta-translation-using-deepl',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '40741fa7ebefa03cf355acb133501ac5f772e907',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/addon-for-post-meta-translation-using-deepl' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '40741fa7ebefa03cf355acb133501ac5f772e907',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
